012f359fb7d4746050d3833bcf4f88b77e14d79e
Ritesh Gandalwad <gandalwadritesh90@gmail.com>
2024-01-23 21:46:39 +0530

    Header Added

--- job-spot/src/App.jsx
+++ job-spot/src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar"
-
+import Header from "./components/Header"
 
 function App() {
 
@@ -7,6 +7,7 @@ function App() {
     <>
       
       <Navbar/>
+      <Header/>
     </>
   )
 }
